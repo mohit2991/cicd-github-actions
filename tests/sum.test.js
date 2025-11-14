b057f82d55fdb89e4379d4497ea6a886486fe43c
Mohit Chauhan <mohitchauhan@Mohits-MacBook-Pro.local>
2025-11-14 20:46:30 +0530

    Add workflows

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -3,7 +3,7 @@ import {describe, expect, test} from "vitest";
 import { sum } from "../src/sum";
 
 describe("Sum", ()=> {
-    test("Sum of two number", ()=> {
+    test("Sum of two numbers", ()=> {
         expect(sum(1,2)).toBe(3);
     })
 })
